c900310db6c11e056a467dbb660c4319e4285af3
sam1016yu <miaoyu1016@gmail.com>
2021-11-11 12:37:02 -0700

    update set_env_variable

--- set_env_variable.sh
+++ set_env_variable.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 sudo echo 'LD_LIBRARY_PATH="/users/miaoyu/calvin/ext/protobuf/src/.libs:/users/miaoyu/calvin/ext/zookeeper/.libs:/users/miaoyu/calvin/ext/googletest/lib/.libs:/usr/local/lib:/usr/lib:/users/miaoyu/calvin/ext/zeromq/lib"' >> /etc/environment
 sudo source /etc/environment
